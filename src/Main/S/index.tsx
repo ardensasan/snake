82f05546573feff5b2ccffc08498c31c5cfb086a
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-10-21 08:08:30 +0800

    snap head when rotating

--- src/Main/S/index.tsx
+++ src/Main/S/index.tsx
@@ -1,15 +1,28 @@
 import Konva from "konva";
 import { useEffect, useRef } from "react";
-import { Layer, Rect } from "react-konva";
+import { Layer } from "react-konva";
 
 const S = () => {
   const rectRef: any = useRef();
   const direction = useRef([1, 0]);
+  let segment = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: 50,
+    height: 50,
+    fill: "#00D2FF",
+    stroke: "black",
+    strokeWidth: 0.5,
+  });
+
+  const segments = useRef([{ segment, direction: [1, 0] }]);
   var head = new Konva.Shape({
     sceneFunc: function (context, shape) {
       context.beginPath();
       context.moveTo(50, 25);
       context.lineTo(25, 50);
+      context.lineTo(0, 50);
+      context.lineTo(0, 0);
       context.lineTo(25, 0);
       context.closePath();
       context.fillStrokeShape(shape);
@@ -17,8 +30,8 @@ const S = () => {
     fill: "#00D2FF",
     stroke: "black",
     strokeWidth: 0.5,
-    x: 0,
-    y: 75,
+    x: 75,
+    y: 25,
     offset: {
       x: 25,
       y: 25,
@@ -27,9 +40,13 @@ const S = () => {
 
   useEffect(() => {
     rectRef.current.add(head);
+    rectRef.current.add(segments.current[0].segment);
     var anim = new Konva.Animation(function (frame: any) {
       head.x(head.attrs.x + direction.current[0]);
       head.y(head.attrs.y - direction.current[1]);
+      segments.current.forEach(element => {
+          
+      });
     }, rectRef);
     anim.start();
   });
@@ -37,9 +54,10 @@ const S = () => {
   window.addEventListener("keypress", ({ code }) => {
     if (code === "KeyW") {
       if (
-        (direction.current[0] === 1 && direction.current[1] === 0) || // right
-        (direction.current[0] === -1 && direction.current[1] === 0) //left
+        (direction.current[0] === 1 && direction.current[1] === 0) ||
+        (direction.current[0] === -1 && direction.current[1] === 0)
       ) {
+        head.y(head.attrs.y-50)
         head.rotate(direction.current[0] * -90);
         direction.current = [0, 1];
       }
@@ -48,6 +66,7 @@ const S = () => {
         (direction.current[0] === 1 && direction.current[1] === 0) ||
         (direction.current[0] === -1 && direction.current[1] === 0)
       ) {
+        head.y(head.attrs.y+50)
         head.rotate(direction.current[0] * 90);
         direction.current = [0, -1];
       }
@@ -56,6 +75,7 @@ const S = () => {
         (direction.current[0] === 0 && direction.current[1] === 1) ||
         (direction.current[0] === 0 && direction.current[1] === -1)
       ) {
+        head.x(head.attrs.x-50)
         head.rotate(direction.current[1] * -90);
         direction.current = [-1, 0];
       }
@@ -64,17 +84,14 @@ const S = () => {
         (direction.current[0] === 0 && direction.current[1] === 1) ||
         (direction.current[0] === 0 && direction.current[1] === -1)
       ) {
+        head.x(head.attrs.x+50)
         head.rotate(direction.current[1] * 90);
         direction.current = [1, 0];
       }
     }
   });
 
-  return (
-    <Layer ref={rectRef}>
-      <Rect width={50} height={50} fill="red" />
-    </Layer>
-  );
+  return <Layer ref={rectRef}></Layer>;
 };
 
 export default S;
